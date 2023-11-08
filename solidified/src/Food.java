ffdb04ffc0c1888c3fa81be7a6c23005c09e6284
masihbr <masihbr@gmail.com>
2023-11-08 23:05:31 +0330

    Add changed code in new folder

---
+++ solidified/src/Food.java
@@ -0,0 +1,26 @@
+public class Food implements Product {
+    String foodName;
+    int price;
+    int quantity = 1;
+
+    Food(String foodName, int foodPrice) {
+        this.foodName = foodName;
+        this.price = foodPrice;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price * quantity;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+}
---
+++ solidified/src/Product.java
@@ -0,0 +1,9 @@
+public interface Product {
+    public void setFoodName(String foodName);
+
+    public void setPrice(int price);
+
+    public int getPrice();
+
+    public String getFoodName();
+}
